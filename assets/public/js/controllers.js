79b728802f715a5226b97720f39afabb9017d012
Bao Nguyen Thien <nguyenthienbao0402@gmail.com>
2022-03-15 16:14:33 +0700

    add slider

--- assets/public/js/controllers.js
+++ assets/public/js/controllers.js
@@ -4,6 +4,11 @@ toggleBtns.forEach(btn => {
     btn.addEventListener('change', (e) => {
         const deviceItem = e.path[3];
         deviceItem.classList.toggle('device-item-active');
-        deviceItem.children[1].children[1].textContent = deviceItem.children[1].children[1].textContent == 'Off' ? 'On' : 'Off';
+        if (deviceItem.dataset.item == 'light') {
+            deviceItem.children[1].children[1].textContent = deviceItem.children[1].children[1].textContent == 'Off' ? 'On' : 'Off';
+        }
+        else if (deviceItem.dataset.item == 'fan') {
+            deviceItem.children[1].children[1].textContent = deviceItem.children[1].children[1].textContent == 'Off' ? '1' : 'Off';
+        }
     })
 })
