ef413847c1019e1600cf4338ce3929ad7f53c381
Bao Nguyen Thien <nguyenthienbao0402@gmail.com>
2022-04-29 11:52:22 +0700

    fix my home edit

--- assets/public/js/settings-button.js
+++ assets/public/js/settings-button.js
@@ -19,10 +19,24 @@ $(document).ready(function(){
         $("#nav_myhome").addClass("item_active");
         $("#nav_change_password").removeClass("item_active");
     });
-
+    // <%= session.user ? session.user.name '\'s House': ''%>
     $("#nav_change_password").click(function(){
         $("#nav_edit_profile").removeClass("item_active");
         $("#nav_myhome").removeClass("item_active");
         $("#nav_change_password").addClass("item_active");
     });
 });
+
+// $('#form-edit-my-home').on('submit', (e) => {
+//     e.preventDefault();
+//     const data = $('#form-edit-my-home').serializeArray();
+//     const name = data[0].value;
+//     const address = data[1].value;
+//     console.log(name, address);
+//     $.ajax({
+
+//         success: (res) => {
+
+//         }
+//     })
+// })
